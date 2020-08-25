9973a214c8c050771c07da2b76a9b276f7bb79d7
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-25 17:36:42 +0300

    Created IProduct interface

---
+++ MASBogrim/MASBogrim/IProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MASBogrim
+{
+    public interface IProduct
+    {
+        void PrintProductInformation();
+        string GetName();
+    }
+}
