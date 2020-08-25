60390738bf8acc70936b77dc5a878524bec867d8
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-25 19:42:04 +0300

    Created IAgent interface

---
+++ MASBogrim/MASBogrim/IAgent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MASBogrim
+{
+    public interface IAgent
+    {
+        void ShouldBid();
+        void CalculateNewPrice();
+    }
+}
