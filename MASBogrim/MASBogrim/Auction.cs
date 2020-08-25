142eaebafbcef23dc694ab6db5f252423a7624f1
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-25 17:35:03 +0300

    Created Auction class

---
+++ MASBogrim/MASBogrim/Auction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MASBogrim
+{
+    public class Auction
+    {
+        public string ProductName { get; set; }
+        public string ProductInfo { get; set; }
+        public double StartPrice { get; set; }
+        public double minPriceJump { get; set; }
+        public DateTime StartTime { get; set; }
+        public int Id { get; set; }
+    }
+}
