58e7a2422ff99f0754b24ef52e821dd184cd293a
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-25 17:29:12 +0300

    Starting to work on the code

---
+++ MASBogrim/MASBogrim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MASBogrim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
