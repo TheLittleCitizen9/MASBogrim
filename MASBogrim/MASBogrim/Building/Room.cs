1f94b69334d4e14006116b57d092bc3d0ee04c74
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-25 17:41:56 +0300

    Created Room class

---
+++ MASBogrim/MASBogrim/Building/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MASBogrim.Building
+{
+    public class Room
+    {
+        public RoomType RoomType { get; set; }
+        public double Size { get; set; }
+
+        public Room(RoomType roomType, double size)
+        {
+            RoomType = roomType;
+            Size = size;
+        }
+    }
+}
