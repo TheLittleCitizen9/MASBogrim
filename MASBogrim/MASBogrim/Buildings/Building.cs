a0e90e0c674f36882af1ba77c7ec16b38f6d63f5
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-25 17:56:41 +0300

    Added Building class

---
+++ MASBogrim/MASBogrim/Buildings/Building.cs
@@ -0,0 +1,44 @@
+﻿using MASBogrim.Building;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MASBogrim.Buildings
+{
+    public class Building : IProduct
+    {
+        private string _name;
+        private bool _hasAirConditioning;
+        private bool _hasProtectionRoom;
+        private bool _hasHighwayPath;
+        private bool _hasDisabledPath;
+        private int _numOfBathrooms;
+        private int _numOfDiningRooms;
+        private int _numOfFloors;
+        private List<Room> _rooms;
+
+        public Building(string name, bool hasAirConditioning, bool hasProtectionRoom, bool hasHighwayPath, 
+            bool hasDisabledPath, int numOfBathrooms, int numOfDiningRooms, int numOfFloors, List<Room> rooms)
+        {
+            _name = name;
+            _hasAirConditioning = hasAirConditioning;
+            _hasProtectionRoom = hasProtectionRoom;
+            _hasHighwayPath = hasHighwayPath;
+            _hasDisabledPath = hasDisabledPath;
+            _numOfBathrooms = numOfBathrooms;
+            _numOfDiningRooms = numOfDiningRooms;
+            _numOfFloors = numOfFloors;
+            _rooms = rooms;
+        }
+
+        public void PrintProductInformation()
+        {
+            Console.WriteLine($"Buildig name: {_name}, Number of rooms: {_rooms.Count}");
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+}
