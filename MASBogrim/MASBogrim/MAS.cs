b7ab0309d2bbabd3dcd47ce58acb2c9a5b685b6a
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-25 20:13:18 +0300

    Added ctor to MAS class

--- MASBogrim/MASBogrim/MAS.cs
+++ MASBogrim/MASBogrim/MAS.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace MASBogrim
 {
-    class MAS
+    public class MAS
     {
+        private List<Agent> _agents;
+        private List<Auction> _auctions;
+        private IProduct _product;
+        private int _secondsUntilClosingEntrance;
+        private int _secondsUntilClosingBids;
+        public event Action<IProduct> GetProductInfo;
+        public event Action GetBids;
+        public event Action<int, double> GetWinner;
+
+        public MAS(List<Agent> agents, List<Auction> auctions, 
+            IProduct product, int secondsUntilClosingEntrance, int secondsUntilClosingBids)
+        {
+            _agents = agents;
+            _auctions = auctions;
+            _product = product;
+            _secondsUntilClosingEntrance = secondsUntilClosingEntrance;
+            _secondsUntilClosingBids = secondsUntilClosingBids;
+        }
+
+
     }
 }
