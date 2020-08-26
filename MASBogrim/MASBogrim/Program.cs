02117648ae4d25e03bf408eb41be58eea9340045
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-26 09:39:10 +0300

    Checked that the basic functionality works (no threads yet)

--- MASBogrim/MASBogrim/Program.cs
+++ MASBogrim/MASBogrim/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MASBogrim.Buildings;
+using System;
+using System.Collections.Generic;
 
 namespace MASBogrim
 {
@@ -6,7 +8,17 @@ namespace MASBogrim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Agent agent1 = new Agent(1);
+            Agent agent2 = new Agent(2);
+
+            List<Room> rooms = new List<Room>() { new Room(RoomType.Bedroom, 20) };
+            Building building = new Building("dar", true, true, true, true, 10, 4, 8, rooms);
+
+            Auction auction = new Auction("dar", "a great house", 10000, 1000, DateTime.Now.AddSeconds(10), 8);
+
+            MAS mas = new MAS(auction, new List<Agent>() { agent1, agent2 }, null, building, 15, 5);
+
+            mas.StartAuction();
         }
     }
 }
