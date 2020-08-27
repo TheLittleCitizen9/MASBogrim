cbf02d3fb4d6247a30f2dfc39c33acdc4156d99e
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-27 11:56:15 +0300

    Added another agent and auction to check if it still works

--- MASBogrim/MASBogrim/Program.cs
+++ MASBogrim/MASBogrim/Program.cs
@@ -10,18 +10,22 @@ namespace MASBogrim
         {
             Agent agent1 = new Agent(1);
             Agent agent2 = new Agent(2);
+            Agent agent3 = new Agent(3);
 
             List<Room> rooms = new List<Room>() { new Room(RoomType.Bedroom, 20) };
-            Building building1 = new Building("dar", true, true, true, true, 10, 4, 8, rooms);
-            Building building2 = new Building("dror", true, true, true, true, 10, 4, 8, rooms);
+            Building building1 = new Building("Hilton", true, true, true, true, 10, 4, 8, rooms);
+            Building building2 = new Building("Cramim", true, true, true, true, 10, 4, 8, rooms);
+            Building building3 = new Building("Bereshit", true, true, true, true, 10, 4, 8, rooms);
 
             Auction auction = new Auction(building1, 10000, 1000, DateTime.Now.AddSeconds(10), 8, 4, 3);
-            Auction auction2 = new Auction(building2, 10000, 1000, DateTime.Now.AddSeconds(10), 6, 4, 3);
+            Auction auction2 = new Auction(building2, 10000, 1000, DateTime.Now.AddSeconds(10), 2, 4, 3);
+            Auction auction3 = new Auction(building3, 10000, 1000, DateTime.Now.AddSeconds(10), 10, 4, 3);
 
             auction.Agents = new List<Agent>() { agent1, agent2 };
             auction2.Agents = new List<Agent>() { agent1, agent2 };
+            auction3.Agents = new List<Agent>() { agent1, agent2, agent3 };
 
-            MAS mas = new MAS(new List<Auction>() { auction, auction2});
+            MAS mas = new MAS(new List<Auction>() { auction, auction2, auction3});
 
             mas.Manager();
         }
